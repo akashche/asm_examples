8590ffaa57aa6ef75450d69147d5caa89e578365
akashche <akashche@redhat.com>
2015-07-17 18:30:14 +0000

    sums and fib added

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-NAME:=hello
+NAME := hello
 
 all: assemble run
 
--- hello/hello.s
+++ hello/hello.s
@@ -12,15 +12,11 @@
 .type main, %function
 main:
     push {r7, lr}
-    sub sp, sp, #8
-    mov r7, sp
 
     ldr r0, =.Lhelloptr
     ldr r0, [r0]
     bl puts
 
-    add r7, r7, #8
-    mov sp, r7
     pop {r7, lr}
     bx lr
     .size main, .-main
