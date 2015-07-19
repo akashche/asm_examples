ee67c35f7f108db8022bcafce12714d7aad84170
akashche <akashche@redhat.com>
2015-07-19 01:02:48 +0000

    bf '>' op jitted

--- bfjit/bfjit.s
+++ bfjit/bfjit.s
@@ -1,18 +1,113 @@
 .syntax unified
 .thumb
 
+.data
+.align 2
+/* ldr.w r1, [sp, #4] */
+.Lline1: .word 0x1004f8dd
+
+.align 2
+/* ldr r3, [r1] */
+.Lline2: .word 0xf103680b
+
+.align 2
+/* add r3, #1 */
+.Lline3: .word 0x600b0301
+
+.align 2
+/* str r3, [r1] */
+/* bx lr */
+.Lline4: .word 0xbf004770
+
 .text
+
+.align 2
+.Lline1_ptr: .word .Lline1
+.align 2
+.Lline2_ptr: .word .Lline2
+.align 2
+.Lline3_ptr: .word .Lline3
+.align 2
+.Lline4_ptr: .word .Lline4
 
 .align 2
 .global op62
 .thumb_func
 .type op62, %function
 op62:
-    ldr r3, [r1]
-    add r3, #1
-    str r3, [r1]
-    mov r0, #0
-    bx lr
+    push {r1, r3, r7, lr}
+    add r7, sp, #16
+
+    sub sp, sp, #4
+
+    mov r0, sp
+    mov r1, #4096
+    mov r2, #16
+    bl posix_memalign
+    cmp r0, #0
+    it ne
+    bne .Lop62exit
+
+    ldr r0, [sp]
+    mov r1, #16
+    mov r2, #0x3
+    bl mprotect
+    cmp r0, #0
+    it ne
+    bne .Lop62exit
+
+    ldr r3, [sp]
+
+    ldr r2, =.Lline1_ptr
+    ldr r2, [r2]
+    ldr r2, [r2]
+    str r2, [r3]
+
+    ldr r2, =.Lline2_ptr
+    ldr r2, [r2]
+    ldr r2, [r2]
+    str r2, [r3, #4]
+
+    ldr r2, =.Lline3_ptr
+    ldr r2, [r2]
+    ldr r2, [r2]
+    str r2, [r3, #8]
+
+    ldr r2, =.Lline4_ptr
+    ldr r2, [r2]
+    ldr r2, [r2]
+    str r2, [r3, #12]
+
+    ldr r0, [sp]
+    mov r1, #16
+    mov r2, #0x5
+    bl mprotect
+    cmp r0, #0
+    it ne
+    bne .Lop62exit
+
+    ldr r0, [sp]
+    ldr r1, [sp, #16]
+    bl __clear_cache
+
+    ldr r3, [sp]
+    add r3, r3, #1
+    blx r3
+
+    ldr r0, [sp]
+    mov r1, #16
+    mov r2, #0x3
+    bl mprotect
+ 
+    ldr r0, [sp]
+    bl free
+
+    .Lop62exit:
+        sub r7, r7, #16
+        mov sp, r7
+        pop {r1, r3, r7, lr}
+        mov r0, #0
+        bx lr
 .size , .-op62
 
 .align 2
@@ -156,7 +251,8 @@ main:
     movle r0, #1
     ble .Lexit
 
-    sub sp, sp, #12
+    sub sp, sp, #16
+
     mov r3, #1
     lsl r3, r3, #8
     add r3, #12
@@ -177,6 +273,7 @@ main:
         cmp r3, r2
         it ls
         bls .Lloopclean
+
 
     .Lloopev:
         ldr r3, [sp]
